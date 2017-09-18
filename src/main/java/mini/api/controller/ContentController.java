0b9e7a6083e962aee88aafdce28dc1a373f3fb3e
kang min <kimkm212@gmail.com>
2017-09-18 17:12:56 +0900

    config cors

--- src/main/java/mini/api/controller/ContentController.java
+++ src/main/java/mini/api/controller/ContentController.java
@@ -7,10 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -22,34 +19,35 @@ public class ContentController {
     private ContentRepository cr;
 
 
-    @RequestMapping(value = "/list/{page}", method = RequestMethod.GET)
+    @GetMapping(value = "/list/{page}")
     public Page<Content> list(@PathVariable int page) {
-        cr.save(new Content("한글이다 ","한글"));
+        //cr.save(new Content("한글이다 ","한글"));
         PageRequest request = new PageRequest(page - 1, 10, Sort.Direction.DESC, "id");
         Page<Content> list = cr.findAll(request);
         return list;
     }
 
+    @PostMapping(value = "/")
+    public Content write(Content ct) {
+        return cr.save(ct);
+    }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    @GetMapping(value = "/{id}")
     public Content view(@PathVariable int id) {
         return cr.findOne(id);
     }
 
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+    @PutMapping(value = "/{id}")
     public Content update(@PathVariable int id, Content ct) {
         ct.setId(id);
         return cr.save(ct);
     }
 
-    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    @DeleteMapping(value = "/{id}")
     public void delete(@PathVariable int id) {
         cr.delete(id);
     }
 
-    @RequestMapping(value = "/", method = RequestMethod.POST)
-    public Content write(Content ct) {
-        return cr.save(ct);
-    }
+
 }
